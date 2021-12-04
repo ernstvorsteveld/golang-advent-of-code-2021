0709c2dceca3e43e2cd48c80bc04020dd59ba3fe
Ernst Vorsteveld <ernst.vorsteveld@stern-itc.com>
2021-12-04 20:03:10 +0100

    renamed and moved

--- challenge1a_test.go
+++ challenge1a_test.go
@@ -2,17 +2,17 @@ package main
 
 import "testing"
 
-func TestExample1(t *testing.T) {
+func TestExample1a(t *testing.T) {
 	const expected = 7
-	var actual = Challenge1([]int{199, 200, 208, 210, 200, 207, 240, 269, 260, 263})
+	var actual = Challenge1a([]int{199, 200, 208, 210, 200, 207, 240, 269, 260, 263})
 	if actual != expected {
 		t.Errorf("Number larger than previous incorrect, got: %d, want: %d.", actual, expected)
 	}
 }
 
-func TestChallenge1(t *testing.T) {
+func TestChallenge1a(t *testing.T) {
 	const expected = 1696
-	var actual = Challenge1File("puzzle-input/puzzle-input-depth-2-measurements.txt")
+	var actual = Challenge1aFile("puzzle-input/1a-depth-measurements.txt")
 	if actual != expected {
 		t.Errorf("Number larger than previous incorrect, got: %d, want: %d.", actual, expected)
 	}
--- challenge1b_test.go
+++ challenge1b_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestExample2(t *testing.T) {
 	const expected = 5
-	var actual = Challenge2([]int{199, 200, 208, 210, 200, 207, 240, 269, 260, 263})
+	var actual = Challenge1b([]int{199, 200, 208, 210, 200, 207, 240, 269, 260, 263})
 	if actual != expected {
 		t.Errorf("Number larger than previous incorrect, got: %d, want: %d.", actual, expected)
 	}
@@ -13,7 +13,7 @@ func TestExample2(t *testing.T) {
 
 func TestChallenge2(t *testing.T) {
 	const expected = 1737
-	var actual = Challenge2File("2/2-measurements.txt")
+	var actual = Challenge1bFile("puzzle-input/1b-measurements.txt")
 	if actual != expected {
 		t.Errorf("Number larger than previous incorrect, got: %d, want: %d.", actual, expected)
 	}
