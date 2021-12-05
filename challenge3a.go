df5220f7b7ff5e1e4f13e5464bfd8c2314c9f9ea
Ernst Vorsteveld <ernst.vorsteveld@stern-itc.com>
2021-12-05 09:43:21 +0100

    made a function for counting on a position

--- challenge3a.go
+++ challenge3a.go
@@ -2,7 +2,7 @@ package main
 
 import "strconv"
 
-func Challenge3aFile(s string) (int64,int64) {
+func Challenge3aFile(s string) (int64, int64) {
 	var commands = ReadMeasurements(s)
 	return Challenge3a(commands)
 }
@@ -10,16 +10,23 @@ func Challenge3aFile(s string) (int64,int64) {
 func Challenge3a(commands []string) (int64, int64) {
 	length := len(commands[0])
 	numberOnes := make([]int, length)
-	for i := 0; i < len(commands); i++ {
-		for j, c := range commands[i] {
-			if c == '1' {
-				numberOnes[j]++
-			}
-		}
+	for i := 0; i < length; i++ {
+		numberOnes[i] = CountBits(commands, i, "1")
 	}
 	g := GammaRate(numberOnes, len(commands))
 	e := EpsilonRate(numberOnes, len(commands))
 	return g, e
+}
+
+func CountBits(commands []string, p int, c string) int {
+	numberOnes := 0
+	for i := 0; i < len(commands); i++ {
+		c := commands[i][p]
+		if c == '1' {
+			numberOnes++
+		}
+	}
+	return numberOnes
 }
 
 func GammaRate(n []int, length int) int64 {
