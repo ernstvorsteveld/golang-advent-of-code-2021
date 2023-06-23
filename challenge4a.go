b7a821cb86ddc62c72edd3783281ad1fb40269af
Ernst Vorsteveld <ernst.vorsteveld@stern-itc.com>
2023-06-23 12:08:11 +0200

    now

--- challenge4a.go
+++ challenge4a.go
@@ -5,24 +5,85 @@ import (
 	"strings"
 )
 
-type Board struct {
-	grid   [][]int
-	scores [][]bool
+type BingoBoard struct {
+	grid [][]BingoField
+}
+
+type BingoField struct {
+	value int
+	draw  bool
 }
 
 type Bingo struct {
+	size   int
 	draws  []int
-	boards []Board
+	boards []BingoBoard
+}
+
+type BingoPlayer interface {
+	play()
+	draw(draw int) (bool, BingoBoard, int, int)
+	check() (bool, BingoBoard, int, int)
+	value() int
+}
+
+func (b Bingo) draw(draw int) (bool, BingoBoard, int, int) {
+	for _, board := range b.boards {
+		for _, h := range board.grid {
+			for _, v := range h {
+				v.draw = v.draw || v.value == draw
+			}
+		}
+	}
+	return b.check()
+}
+
+func (b Bingo) check() (bool, BingoBoard, int, int) {
+	for _, board := range b.boards {
+		for i, h := range board.grid {
+			allHorizontals := false
+			for _, v := range h {
+				allHorizontals = allHorizontals && v.draw
+			}
+			if allHorizontals {
+				return true, board, i, 0
+			}
+		}
+	}
+	for _, board := range b.boards {
+		for i := 0; i < b.size; i++ {
+			allVerticals := false
+			j := 0
+			for ; j < b.size; j++ {
+				allVerticals = allVerticals && board.grid[j][i].draw
+			}
+			if allVerticals {
+				return true, board, j, i
+			}
+		}
+	}
+	return false, BingoBoard{}, 0, 0
+}
+
+func (b Bingo) value() int {
+	return 0
 }
 
 func Challenge4aSimple(f string, size int) int {
 	lines := ReadMeasurements(f)
-	game := Bingo{ReadDraws(lines[0]), ReadBoards(lines, size)}
+	game := Bingo{size, ReadDraws(lines[0]), ReadBoards(lines, size)}
 	return PlayBingo(game)
 }
 
-func PlayBingo(game Bingo) int {
-	return 0
+func PlayBingo(play BingoPlayer) int {
+	play.play()
+	return play.value()
+}
+
+func (b Bingo) play() {
+	for _, s := range b.draws {
+		b.draw(s)
+	}
 }
 
 func ReadDraws(s string) []int {
@@ -44,14 +105,14 @@ func ReadLineAsInteger(length int, drawsString []string) []int {
 	return draws
 }
 
-func ReadBoards(lines []string, size int) []Board {
-	boards := make([]Board, 0)
+func ReadBoards(lines []string, size int) []BingoBoard {
+	boards := make([]BingoBoard, 0)
 	remainingBoardLines := lines[2:]
 	for len(remainingBoardLines) >= size {
 		board := ReadBoard(remainingBoardLines[:size], size)
 		boards = append(boards, board)
 
-		if len(remainingBoardLines) > size + 1 {
+		if len(remainingBoardLines) > size+1 {
 			remainingBoardLines = remainingBoardLines[size+1:]
 		} else {
 			remainingBoardLines = remainingBoardLines[size:]
@@ -60,21 +121,19 @@ func ReadBoards(lines []string, size int) []Board {
 	return boards
 }
 
-func ReadBoard(lines []string, size int) Board {
-	var gridValues [][]int = make([][]int, size)
-	var gridBools [][]bool = make([][]bool, size)
+func ReadBoard(lines []string, size int) BingoBoard {
+	var bingoFields = make([][]BingoField, size)
 	for k := 0; k < size; k++ {
-		gridValues[k] = make([]int, size)
-		gridBools[k] = make([]bool, size)
+		bingoFields[k] = make([]BingoField, size)
 	}
 
 	for i := 0; i < len(lines); i++ {
 		lineAsStrings := strings.Split(lines[i], " ")
-		line:= ReadLineAsInteger(size, lineAsStrings)
+		line := ReadLineAsInteger(size, lineAsStrings)
 		for j := 0; j < size; j++ {
-			gridValues[i][j] = line[j]
-			gridBools[i][j] = false
+			bingoFields[i][j].value = line[j]
+			bingoFields[i][j].draw = false
 		}
 	}
-	return Board{grid: gridValues, scores: gridBools}
+	return BingoBoard{grid: bingoFields}
 }
